f16c68c32ac312ae3da8feb05fb3f64525c9db23
Sophia Otis <sophianotis3@gmail.com>
2024-04-25 21:57:05 -0600

    yep

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// inject current year in footer
+const numberOne = new Date();
+document.querySelector("#year").textContent = numberOne.getFullYear();
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+
+x.onclick = toggleMenu;
